894d16b6092bd00a1cb1073c9ddf04376a50ba14
Murton Liu <36607410+liumurto@users.noreply.github.com>
2018-02-18 18:00:39 -0500

    Add files via upload

--- Lab2/sharedssl.h
+++ Lab2/sharedssl.h
@@ -1,1 +1,15 @@
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+void init_ssl(void);
+int init_certs(SSL_CTX *ssl_ctx, char *cert_name);
+void show_cert(SSL* ssl);
